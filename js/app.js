ae35a383a3dda8635987545d9c46b00e7698f8e0
Nicole Espinosa <107057243+nicoleespinosa@users.noreply.github.com>
2022-08-22 12:43:10 -0300

    add js and thanks page via upload

---
+++ js/app.js
@@ -0,0 +1,102 @@
+const header = document.querySelector("header");
+
+const first_skill = document.querySelector(".skill:first-child");
+const sk_counters = document.querySelectorAll(".counter span");
+const progress_bars = document.querySelectorAll(".skills svg circle");
+
+const links = document.querySelectorAll(".nav-link");
+
+window.addEventListener("scroll", () => {
+  activeLink();
+  if (!skillsPlayed) skillsCounter();
+});
+/*----------------- Sticky Navbar----------------*/
+
+function stickyNavbar() {
+  header.classList.toggle("scrolled", window.pageYOffset > 0);
+}
+
+stickyNavbar();
+
+window.addEventListener("scroll", stickyNavbar);
+
+/*----------------- Reveal Animation----------------*/
+
+let sr = ScrollReveal( {
+  duration: 2500,
+  distance: "60px",
+});
+
+sr.reveal(".showcase-info", {delay: 200});
+sr.reveal(".showcase-image", { origin: "top", delay: 100});
+
+
+/*----------------- Skills Progress Bar Animation----------------*/
+function hasReached(el) {
+  let topPosition = el.getBoundingClientRect().top;
+  
+  if(window.innerHeight >= topPosition + el.offsetHeight) return true; return false;
+}
+
+function updateCount(num, maxNum) {
+  let currentNum = +num.innerText;
+  
+  if(currentNum < maxNum) {
+    num.innerText = currentNum + 1;
+    setTimeout(() => {
+      updateCount(num, maxNum);
+    }, 12);
+  }
+}
+
+let skillsPlayed = false;
+
+function skillsCounter() {
+  if(!hasReached(first_skill)) return;
+
+  skillsPlayed = true;
+
+  sk_counters.forEach((counter, i) => {
+    let target = +counter.dataset.target;
+    let strokeValue = 427 - 427 * (target / 100);
+
+    progress_bars[i].style.setProperty("--target", strokeValue);
+
+    setTimeout(() => {
+      updateCount(counter, target);
+    }, 400);
+  })
+
+  progress_bars.forEach((p) => (p.style.animation = "progress 2s ease-in-out forwards"));
+}
+
+/*----------------- Portfolio Filter Animation----------------*/
+
+let mixer = mixitup(".portfolio-gallery", {
+  selectors: {
+      target: '.prt-card'
+  },
+  animation: {
+      duration: 800,
+  }
+});
+
+/*------------- Change Active Link On Scroll----------------*/
+
+function activeLink () {
+  let sections = document.querySelectorAll("section[id]");
+  let passedSections = Array.from(sections).map((sct, i) => {
+    return { 
+      y: sct.getBoundingClientRect().top - header.offsetHeight,
+      id: i,
+    };
+  })
+  .filter(sct => sct.y <= 0);
+
+  let currSectionID = passedSections.at(-1).id;
+  
+  links.forEach(l => l.classList.remove("active"));
+  links[currSectionID].classList.add("active");
+}
+
+activeLink();
